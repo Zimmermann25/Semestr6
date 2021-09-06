35eaaa40232f214ca29528e29671545fedcdd01d
Zimmermann25 <80169648+Zimmermann25@users.noreply.github.com>
2021-09-06 20:41:14 +0200

    Delete sudokuAndroid directory

--- sudokuAndroid/sudoku1/game/Cell.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sudoku1.game
-
-class Cell(val row:Int, val col: Int, var value:Int,
-           var isStartingCell: Boolean = false,
-           var notes: MutableSet<Int> = mutableSetOf<Int>()) // val to store it in a class, set beacuse of notes will be unique
--- sudokuAndroid/sudoku1/viewmodel/PlaySudokuViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sudoku1.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.sudoku1.game.SudokuGame
-
-class PlaySudokuViewModel : ViewModel(){
-    val sudokuGame = SudokuGame()
-}
-
